614f65ae7ca0c6bd7ac65bef77db0fb26f409a01
Tomas Barton <barton.tomas@gmail.com>
2024-03-11 09:49:23 +0100

    Declare cache directory only once (#2)

--- spec/classes/pubkey_spec.rb
+++ spec/classes/pubkey_spec.rb
@@ -4,13 +4,13 @@ require 'spec_helper'
 
 describe 'pubkey' do
   _, os_facts = on_supported_os.first
-  context "with default parameters" do
+  context 'with default parameters' do
     let(:facts) { os_facts }
 
     it { is_expected.to compile.with_all_deps }
   end
 
-  context 'multiple keys' do
+  context 'multiple keys with custom dir' do
     let(:facts) { os_facts }
     let :pre_condition do
       <<-PP
@@ -28,6 +28,24 @@ describe 'pubkey' do
       PP
     end
 
+    exported_keys = '/var/cache/pubkey/exported_keys'
     it { is_expected.to compile.with_all_deps }
+    it { is_expected.to contain_file('/var/cache/pubkey').with_ensure('directory') }
+    it { is_expected.to contain_file(exported_keys).with_ensure('file') }
+
+    it { is_expected.to contain_pubkey__ssh('johndoe_ed25519') }
+    it { is_expected.to contain_pubkey__ssh('johndoe_ed25519-sk') }
+
+    it {
+      is_expected.to contain_file_line('johndoe:/home/johndoe/.ssh/id_ed25519.pub').with(
+        path: exported_keys,
+      )
+    }
+
+    it {
+      is_expected.to contain_file_line('johndoe:/home/johndoe/.ssh/id_ed25519_sk.pub').with(
+        path: exported_keys,
+      )
+    }
   end
 end

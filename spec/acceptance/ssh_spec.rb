876a06eefa2417c124032e4ed0fe9e3061ae7f38
Tomas Barton <barton.tomas@gmail.com>
2024-03-12 11:32:11 +0100

    Add specs for sk keys (#4)

--- spec/acceptance/ssh_spec.rb
+++ spec/acceptance/ssh_spec.rb
@@ -90,4 +90,48 @@ describe 'pubkey::ssh' do
       its(:stdout) { is_expected.to match "john:/home/john/.ssh/id_dsa.pub\n" }
     end
   end
+
+  context 'secure key' do
+    it 'generate ssh key' do
+      pp = <<~EOS
+        pubkey::ssh { 'john_ed25519-sk': }
+      EOS
+
+      expect(apply_manifest(pp, {
+                              catch_failures: false,
+                              debug: false,
+                            }).exit_code).to be_zero
+
+      expect(apply_manifest(pp, {
+                              catch_failures: false,
+                              debug: false,
+                            }).exit_code).to be_zero
+    end
+
+    describe file('/home/john/.ssh') do
+      it { is_expected.to be_directory }
+      it { is_expected.to be_readable.by('owner') }
+      it { is_expected.not_to be_readable.by('group') }
+      it { is_expected.not_to be_readable.by('others') }
+    end
+
+    describe file('/home/john/.ssh/id_ed25519_sk') do
+      it { is_expected.to be_file }
+      it { is_expected.to be_readable.by('owner') }
+      it { is_expected.not_to be_readable.by('group') }
+      it { is_expected.not_to be_readable.by('others') }
+    end
+
+    describe file('/home/john/.ssh/id_ed25519_sk.pub') do
+      it { is_expected.to be_file }
+      it { is_expected.to be_readable.by('owner') }
+      it { is_expected.to be_readable.by('group') }
+      it { is_expected.to be_readable.by('others') }
+    end
+
+    describe command('cat /var/cache/pubkey/exported_keys') do
+      its(:exit_status) { is_expected.to eq 0 }
+      its(:stdout) { is_expected.to match "john:/home/john/.ssh/id_ed25519_sk.pub\n" }
+    end
+  end
 end
